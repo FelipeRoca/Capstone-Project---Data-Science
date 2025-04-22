366940ee0c96cf5e042adbe26937337ac5baf080
FelipeRoca <feliroca01@gmail.com>
2025-04-22 17:18:05 -0300

    Graficas para ver proporcion de compra de clientes bajo determinados parametros. Preprocesamiento de destinatarios

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
+import seaborn as sns
 
 
 from sklearn.impute import SimpleImputer
@@ -120,3 +121,170 @@ print(classification_report(y_test, y_pred))
 # Accuracy es la proporción de todas las predicciones (tanto para la clase 0 como para la clase 1) que fueron correctas. En este caso, el modelo tuvo una precisión general del 73%
 # Macro avg es el promedio simple de la precisión, el recall y el f1-score entre las dos clases
 # Weighted avg: Es el promedio ponderado de la precisión, el recall y el f1-score entre las dos clases, donde el peso es el soporte (el número de instancias reales en cada clase)
+
+
+
+#   GRAFICAS DE ESTUDIO DE CLIENTES
+
+# estado civil vs compra bicis
+plt.subplot(2, 2, 1)
+sns.countplot(x='EstadoCivil', hue='ComproBicicleta', data=clientes)
+plt.title('Estado Civil vs Compro Bicicleta')
+plt.xlabel('Estado Civil (0=Casado, 1=Soltero)')
+plt.ylabel('Cantidad')
+
+
+# hijos vs compra bici
+plt.subplot(2, 2, 2)
+sns.countplot(x='TotalHijos', hue='ComproBicicleta', data=clientes)
+plt.title('Total de Hijos vs Compro Bicicleta')
+plt.xlabel('Cantidad de Hijos')
+plt.ylabel('Cantidad')
+
+# cantidad de autos vs Compra bici
+plt.subplot(2, 2, 3)
+sns.countplot(x='CantAutomoviles', hue='ComproBicicleta', data=clientes)
+plt.title('Cantidad de Automóviles vs Compro Bicicleta')
+plt.xlabel('Cantidad de Automóviles')
+plt.ylabel('Cantidad')
+
+# distancia al trabajo vs compra bicis
+plt.subplot(2, 2, 4)
+# redondeamos p agrupar por distancia entera
+clientes['DistanciaRedondeada'] = clientes['Distancia'].round()
+sns.countplot(x='DistanciaRedondeada', hue='ComproBicicleta', data=clientes)
+plt.title('Distancia al trabajo vs Compro Bicicleta')
+plt.xlabel('Distancia al trabajo (km)')
+plt.ylabel('Cantidad')
+
+# grafico matriz correlacion
+plt.figure(figsize=(12, 8))
+sns.heatmap(clientes.corr(), annot=True, cmap='coolwarm')
+plt.title('Matriz de Correlación entre Variables Numéricas')
+
+
+
+
+
+
+# tasa de compra por grupo en eatado civil
+conversion_estado = clientes.groupby('EstadoCivil')['ComproBicicleta'].mean()
+print(conversion_estado)
+# el 37% de los solteros compro bicicletas
+# el 42% de los casados compra bicicletas
+plt.figure(figsize=(10, 6))
+sns.barplot(x=conversion_estado.index, y=conversion_estado.values)
+plt.title('Tasa de compra por estado civil')
+plt.xlabel('Estado Civil')
+plt.ylabel('Tasa de compra')
+
+
+
+# tasa de compra por grupo en hijos
+conversion_hijos = clientes.groupby('TotalHijos')['ComproBicicleta'].mean()
+print(conversion_hijos)
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x=conversion_hijos.index, y=conversion_hijos.values)
+plt.title('Tasa de compra por cantidad de hijos')
+plt.xlabel('Cantidad de hijos')
+plt.ylabel('Tasa de compra')
+
+
+
+# tasa de compra por grupo en cantidad de autos
+conversion_autos = clientes.groupby('CantAutomoviles')['ComproBicicleta'].mean()
+print(conversion_autos)
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x=conversion_autos.index, y=conversion_autos.values)
+plt.title('Tasa de compra por cantidad de autos')
+plt.xlabel('Cantidad de autos')
+plt.ylabel('Tasa de compra')
+
+
+# tasa de compra por grupo en distancia
+conversion_distancia = clientes.groupby('Distancia')['ComproBicicleta'].mean()
+print(conversion_distancia)
+# el 46% de los que viven a menos de 1km compro
+# el 36 de los que viven 1-2km
+# el 46% de los de 2-5km
+# el 32% de los de 5-10km
+# el 28 de los de +10km
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x=conversion_distancia.index, y=conversion_distancia.values)
+plt.title('Tasa de compra por distancia')
+plt.xlabel('Distancia')
+plt.ylabel('Tasa de compra')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#       EVALUACION DE LOS DESTINATARIOS
+
+# print(destinatarios.head())
+
+
+# eliminamos las columnas q no se necesitan
+
+destinatarios.drop("IdCiudad" , axis=1, inplace=True)
+destinatarios.drop("Nombre" , axis=1, inplace=True)
+destinatarios.drop("Apellido" , axis=1, inplace=True)
+destinatarios.drop("Telefono" , axis=1, inplace=True)
+destinatarios.drop("Email" , axis=1, inplace=True)
+destinatarios.drop("Direccion" , axis=1, inplace=True)
+destinatarios.drop("FechaNacimiento" , axis=1, inplace=True)
+
+#usamos una logica parecida a la de clientes para limpiar la columnba de distancia
+
+destinatarios['Distancia'] = destinatarios['Distancia'].str.replace("'", "") # eliminar comillas 
+destinatarios['Distancia'] = destinatarios['Distancia'].str.replace(" Km.", "") # eliminar " Km."
+
+destinatarios['Distancia'] = destinatarios['Distancia'].apply(convertir_distancia)
+
+
+# print(destinatarios.head())
+
+# destinatarios.info()
+label_encoders = {}
+for column in ['EstadoCivil', 'Genero', 'Educacion', 'Ocupacion', 'Region']:
+    le = LabelEncoder()
+    destinatarios[column] = le.fit_transform(destinatarios[column])
+    label_encoders[column] = le # Guardar los encoders para usar luego en destinatarios
+
+
+
